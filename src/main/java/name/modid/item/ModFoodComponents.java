0682b39e23b5a05383dbf5664b0afaa5257c92f5
jadooDev <jordymartinez258@gmail.com>
2024-10-12 17:25:13 -0400

    update mod

--- src/main/java/name/modid/item/ModFoodComponents.java
+++ src/main/java/name/modid/item/ModFoodComponents.java
@@ -9,7 +9,14 @@ public class ModFoodComponents
     public static final FoodComponent COAL_APPLE = new FoodComponent.Builder()
             .nutrition(4)
             .saturationModifier(0.6f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 1200), 0.6f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 300), 1.0f)
+            .alwaysEdible()
+            .build();
+
+    public static final FoodComponent COPPER_APPLE = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(0.6f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 1200), 0.3f)
             .alwaysEdible()
             .build();
 
@@ -17,9 +24,50 @@ public class ModFoodComponents
             .nutrition(4)
             .saturationModifier(1.5f)
             .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 2400, 1), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 2), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 2), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 400), 1.0f)
             .alwaysEdible()
             .build();
 
+    public static final FoodComponent EMERALD_APPLE = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(1.5f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 2400, 1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.LUCK, 2400), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 140, 1), 1.0f)
+            .alwaysEdible()
+            .build();
 
+    public static final FoodComponent IRON_APPLE = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 1200), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 60), 1.0f)
+            .alwaysEdible()
+            .build();
+
+    public static final FoodComponent LAPIS_APPLE = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(0.6f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.WATER_BREATHING, 1200), 1.0f)
+            .alwaysEdible()
+            .build();
+
+    public static final FoodComponent REDSTONE_APPLE = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(0.6f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SATURATION, 5), 1.0f)
+            .alwaysEdible()
+            .build();
+
+    public static final FoodComponent NETHERITE_APPLE = new FoodComponent.Builder()
+            .nutrition(4)
+            .saturationModifier(2.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 2400, 2), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 4800), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 10000000), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 3), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 1200, 1), 1.0f)
+            .alwaysEdible()
+            .build();
 }
--- src/main/java/name/modid/item/ModItems.java
+++ src/main/java/name/modid/item/ModItems.java
@@ -11,7 +11,13 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item COAL_APPLE = registerItem("coal_apple", new Item(new Item.Settings().food(ModFoodComponents.COAL_APPLE)));
+    public static final Item COPPER_APPLE = registerItem("copper_apple", new Item(new Item.Settings().food(ModFoodComponents.COPPER_APPLE)));
     public static final Item DIAMOND_APPLE = registerItem("diamond_apple", new Item(new Item.Settings().food(ModFoodComponents.DIAMOND_APPLE)));
+    public static final Item EMERALD_APPLE = registerItem("emerald_apple", new Item(new Item.Settings().food(ModFoodComponents.EMERALD_APPLE)));
+    public static final Item IRON_APPLE = registerItem("iron_apple", new Item(new Item.Settings().food(ModFoodComponents.IRON_APPLE)));
+    public static final Item LAPIS_APPLE = registerItem("lapis_apple", new Item(new Item.Settings().food(ModFoodComponents.LAPIS_APPLE)));
+    public static final Item NETHERITE_APPLE = registerItem("netherite_apple", new Item(new Item.Settings().food(ModFoodComponents.NETHERITE_APPLE)));
+    public static final Item REDSTONE_APPLE = registerItem("redstone_apple", new Item(new Item.Settings().food(ModFoodComponents.REDSTONE_APPLE)));
 
     private static Item registerItem(String name, Item item)
     {
@@ -24,7 +30,13 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
             entries.add(COAL_APPLE);
+            entries.add(COPPER_APPLE);
             entries.add(DIAMOND_APPLE);
+            entries.add(EMERALD_APPLE);
+            entries.add(IRON_APPLE);
+            entries.add(LAPIS_APPLE);
+            entries.add(NETHERITE_APPLE);
+            entries.add(REDSTONE_APPLE);
         });
     }
 }
